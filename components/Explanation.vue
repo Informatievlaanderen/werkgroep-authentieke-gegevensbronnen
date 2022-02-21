f1f2d72375fc690672f2b9a376ab2fa7ee72b298
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2022-02-21 16:06:28 +0100

    [skip ci]: Updated introduction

--- components/Explanation.vue
+++ components/Explanation.vue
@@ -2,76 +2,101 @@
   <vl-grid mod-stacked>
     <vl-column>
       <p>
-        Eind 2016 werd het Stuurorgaan Vlaams Informatie- en ICT-beleid
-        opgericht. De bepalingen werden opgenomen in het Bestuursdecreet. De
-        stuurgroep GDI-Vlaanderen, de stuurgroep inzake archivering en het
-        VDI-coördinatiecomité gaan hierin op.
-      </p>
-    </vl-column>
-    <vl-column>
-      <p>
-        Dit Stuurorgaan is het (nieuwe) coördinatiecomité voor de VDI en kan
-        adviezen aangaande het informatie- en ICT-beleid voorstellen aan de
-        Vlaamse Regering. Die adviezen worden bindend eenmaal de Vlaamse
-        Regering ze heeft goedgekeurd en ze gelden dan zowel binnen de Vlaamse
-        overheid zelf als tussen de Vlaamse overheid en de provinciale en lokale
-        besturen.
-      </p>
-    </vl-column>
-    <vl-column>
-      <p>
-        Daarnaast kreeg dit Stuurorgaan de opdracht om de onderlinge afstemming
-        tussen de intra- en interbestuurlijke strategische
-        digitaliseringsprojecten, gebundeld in het programma Vlaanderen Radicaal
-        Digitaal, te regisseren.
+        Eind 2016 werd het Stuurorgaan Vlaamse Informatie- en ICT-beleid
+        opgericht. De bepalingen werden openomgen in het Bestuursdecreet. Meer
+        informatie omtrent het Stuurorgaan vindt u
+        <a
+          href="https://overheid.vlaanderen.be/stuurorgaan-vlaams-informatie-en-ict-beleid"
+          >hier</a
+        >.
       </p>
     </vl-column>
     <vl-column>
       <vl-drawers id="drawer-a">
         <vl-drawer label="Wat is een Vlaamse Authentieke Gegevensbron?">
           <vl-title tag-name="h2">Een authentieke gegevensbron ...</vl-title>
-          <p>
-            is de meest volledige, kwalitatief hoogstaande verzameling van
-            gegevens die op elektronische wijze worden bijgehouden. Authentieke
-            gegevensbronnen zijn nuttig of noodzakelijk bij de uitvoering van de
-            taken van algemeen belang waarmee Vlaamse en lokale overheden belast
-            zijn of bij de uitvoering van de verplichtingen die op hen rusten.
-            De voorwaarden om erkend te worden als Vlaamse Authentieke
-            Gegevensbron worden door de Vlaamse Regering bepaald. Ook kan zij
-            kandidaat authentieke gegevensbronnen aanwijzen op voorstel van het
-            stuurorgaan.
-          </p>
-          <br />
-          <p>
-            Bij elke erkenning als authentieke gegevensbron bepaalt de Vlaamse
-            Regering welke overheidsinstantie de betreffende gegevensbron
-            beheert en onder welke voorwaarden en op welke wijze de gegevens
-            worden verwerkt. Voor bronnen die nog niet aan de voorwaarden ter
-            erkenning voldoen bepaalt de Vlaamse Regering hoe de nodige
-            aanpassingen om tot erkenning te komen worden uitgevoerd en hoe ze
-            worden gefinancierd.
-          </p>
-          <br />
-          <p>
-            De overheidsinstanties die authentieke gegevensbronnen beheren zijn
-            verantwoordelijk voor de instandhouding, de terbeschikkingstelling,
-            de veiligheid, de toegang en het gebruik van de authentieke
-            gegevensbronnen en van de gegevens van de authentieke
-            gegevensbronnen in kwestie. De overheidsinstanties worden door de
-            Vlaamse regering aangeduid.
-          </p>
-          <br />
-          <p>
-            De inwinning van gegevens kan door de bronhouder zelf gebeuren of er
-            kan gewerkt worden via gegevensinitiatoren die gegevens bij de
-            burger opvragen. De Vlaamse Regering kan de modaliteiten voor
-            samenwerking tussen gegevensinitiatoren en de bronbeheerders nader
-            bepalen en beiden daarbij verplichtingen opleggen.
-          </p>
-          <br />
-          <p>
-            <i>bron: bestuursdecreet van 7 december 2018</i>
-          </p>
+          <vl-grid mod-stacked>
+            <vl-column>
+              <p>
+                ... is een gegevensbron die door de Vlaamse Regering erkend is
+                als de meest volledige, kwalitatief hoogstaande verzameling van
+                gegevens die op elektronische wijze worden bijgehouden.
+                Daarenboven zijn authentieke gegevensbronnen nuttig of
+                noodzakelijk voor de Vlaamse overheid en de lokale overheden bij
+                de uitvoeren van taken van algemeen belang waarmee zij belast
+                zijn of bij de uitvoering van de verplichtingen die op hen
+                rusten.
+              </p>
+            </vl-column>
+            <vl-column>
+              <p>
+                Om als authentieke gegevensbron erkend te worden moet de
+                gegevensbron voldoende garantie bieden voor:
+                <vl-icon-list tag-name="ol">
+                  <vl-icon-list-item tag-name="li" icon="bullet">
+                    De kwaliteit van de gegevens, in het bijzonder de
+                    volledigheid, de juistheid, de nauwkeurigheid, de
+                    actualiteit, de garanties voor de kwaliteitsborging van de
+                    gegevens en de kwaliteitsbewaking naar de afnemers toe;
+                  </vl-icon-list-item>
+                  <vl-icon-list-item tag-name="li" icon="bullet">
+                    De bruikbaarheid van de gegevensbron, in het bijzonder de
+                    ontsluitbaarheid, de beschikbaarheid en de openbaarheid;
+                  </vl-icon-list-item>
+                  <vl-icon-list-item tag-name="li" icon="bullet">
+                    Het adequate beheer van de gegevensbron, in het bijzonder de
+                    aanwezigheid van een inzage- en terugmeldfaciliteit;
+                  </vl-icon-list-item>
+                  <vl-icon-list-item tag-name="li" icon="bullet">
+                    De veiligheid van de gegevensbron op fysiek, technisch en
+                    organisatorisch niveau, in het bijzonder de opspoorbaarheid
+                    van de wijzigingen in de gegevens en de bewaring van de
+                    historiek van de toegang tot de gegevens;
+                  </vl-icon-list-item>
+                  <vl-icon-list-item tag-name="li" icon="bullet">
+                    De financiering van de gegevensbron, in het bijzonder de
+                    financiering van de kosten ingevolge de specifieke
+                    dienstverlening die een authentieke gegevensbron aan andere
+                    instanties moet leveren;
+                  </vl-icon-list-item>
+                  <vl-icon-list-item tag-name="li" icon="bullet">
+                    De interoperabiliteit van de gegevensbron, in het bijzonder
+                    de semantische en technische interoperabiliteit.
+                  </vl-icon-list-item>
+                </vl-icon-list>
+              </p>
+            </vl-column>
+            <vl-column>
+              <p>
+                De Vlaamse Regering kan kandidaat authentieke gegevensbronnen
+                aanwijzen op voorstel van het Stuurorgaan Vlaamse Informatie- en
+                ICT-beleid.
+              </p>
+            </vl-column>
+            <vl-column>
+              <p>
+                Bij elke erkenning als authentieke gegevensbron bepaalt de
+                Vlaamse Regering welke overheidsinstantie de betreffende
+                gegevensbron beheert
+              </p>
+            </vl-column>
+            <vl-column>
+              De overheidsinstanties die authentieke gegevensbronnen beheren
+              zijn verantwoordelijk voor de instandhouding, de
+              terbeschikkingstelling, de veiligheid, de toegang en het gebruik
+              van de authentieke gegevensbronnen en van de gegevens van de
+              authentieke gegevensbronnen in kwestie. Ze kunnen daarvoor zelf of
+              via gegevensinitiatoren, gegevens bij de burger opvragen
+            </vl-column>
+            <vl-column>
+              <p style="font-style: italic">
+                bron: artikel III. 66 en III.67 Bestuursdecreet en besluit van
+                de Vlaamse Regering van 15 mei 2009 houdende de uitvoering van
+                artikel III.66, III.67 en III.68 van het bestuursdecreet van 7
+                december 2018
+              </p>
+            </vl-column>
+          </vl-grid>
         </vl-drawer>
       </vl-drawers>
     </vl-column>
